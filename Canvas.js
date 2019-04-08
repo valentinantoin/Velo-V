4592f4eccb9690581ca903b28447e08a443639e7
Valentin <valentin.antoin@gmail.com>
2019-04-08 15:20:54 +0100

    Add mouse motion detection method

--- Canvas.js
+++ Canvas.js
@@ -21,3 +21,31 @@ class Canvas {
         this.signature;
     }
 };
+
+
+//---ADD MOUSE DRAWING METHOD
+Canvas.prototype.initMouse = function() {
+
+    //---ADD EVENT LISTENERS
+    canvas.addEventListener("mousedown", pointerDown);
+    canvas.addEventListener("mouseup", pointerUp);   
+
+
+    //---ADD MOTION DETECTION
+    function pointerDown(e) {
+        ctx.beginPath();
+        ctx.moveTo(e.offsetX, e.offsetY);
+        canvas.addEventListener("mousemove", paint);
+    };
+ 
+    function pointerUp(e) {
+        canvas.removeEventListener("mousemove", paint);
+        paint(e);
+    };
+ 
+    function paint(e) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        Canvas.signature = 1;
+    };
+};

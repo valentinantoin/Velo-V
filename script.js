0f60234ad6a6291ed7e171fe965d3508b299b154
Valentin <valentin.antoin@gmail.com>
2019-04-08 15:25:22 +0100

    Group Slider / Map / Canvas init()

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 // STRICT MODE
 "use strict";
+
+
+document.addEventListener('DOMContentLoaded', function() {
+ 
+ //---SLIDER INIT 
+    var slider = new Slider();
+    slider.next();
+
+//---MAP INIT
+    var myMap = new Map();
+    myMap.init()
+
+//---CANVAS INIT
+    var myCanvas = new Canvas();
+    myCanvas.initMouse();
+    myCanvas.initTouch();    
+});

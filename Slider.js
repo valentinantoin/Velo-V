c8f7dcf9e8fe1eef3a5e236d047c837ae6910aad
Valentin <valentin.antoin@gmail.com>
2019-04-08 15:01:30 +0100

    Add variables declaration

--- Slider.js
+++ Slider.js
@@ -1,2 +1,11 @@
 // SRICT MODE
 "use strict";
+
+//---VARIABLES DECLARATION
+var slide = document.querySelector("#slides img");
+var prev = document.getElementById("control_prev");
+var next = document.getElementById("control_next");
+var pause = document.getElementById("pause");
+var pauseIcon = document.getElementById("pauseIcon");
+var presentation_l = document.getElementById("presentation_l");
+var presentation_r = document.getElementById("presentation_r");

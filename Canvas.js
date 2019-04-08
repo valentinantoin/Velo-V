4137b323a3bc69264cc263c124d3b366e8f7f409
Valentin <valentin.antoin@gmail.com>
2019-04-08 15:19:40 +0100

    Add variables declaration

--- Canvas.js
+++ Canvas.js
@@ -1,2 +1,12 @@
 // STRICT MODE
 "use strict";
+
+
+//---VARIABLE DECLARATIONS
+var canvas = document.getElementById("signatureCanvas"),
+    ctx = canvas.getContext("2d");
+var buttonClear = document.getElementById("buttonClear");
+var buttonAccept = document.getElementById("buttonAccept");
+var resa = document.getElementById("resa");
+var signWarning = document.getElementById("signWarning");
+

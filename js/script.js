2c2c2431dcbf5e26351f37ce910685053566bb23
Valentin <valentin.antoin@gmail.com>
2019-04-18 13:35:42 +0200

    Add reservation method init

--- js/script.js
+++ js/script.js
@@ -4,17 +4,19 @@
 
 //---VERIFY DOM IS FULLY LOADED
 document.addEventListener('DOMContentLoaded', function() {
- 
+
  //---SLIDER INIT 
     var slider = new Slider();
     slider.next();
 
 //---MAP INIT
     var myMap = new Map();
-    myMap.init()
+    myMap.init();
 
 //---CANVAS INIT
     var myCanvas = new Canvas();
     myCanvas.initMouse();
-    myCanvas.initTouch();    
+    myCanvas.initTouch(); 
+    myCanvas.reservation();
+       
 });
